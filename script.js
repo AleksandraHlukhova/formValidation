ece55d7ee2a4df577afa5d859da0a72d315e8a4c
AleksandraHlukhova <a0993715642@gmail.com>
2020-05-02 21:59:42 +0300

    edit error mesage function

--- script.js
+++ script.js
@@ -1,130 +1,140 @@
 
 const nameInput = document.querySelector('[name="name"]'),
-    emailInput = document.querySelector('[name="email"]'),
-    phoneInput = document.querySelector('[name="phone"]'),
-    msgArea = document.querySelector('[name="message"]'),
-    btn = document.querySelector('.btn');
+emailInput = document.querySelector('[name="email"]'),
+phoneInput = document.querySelector('[name="phone"]'),
+msgArea = document.querySelector('[name="message"]'),
+btn = document.querySelector('.btn'),
+form = document.querySelector('form');
 const inputs = document.querySelectorAll('input');
 
 ///// Удаляет поле с ошибкой
 let delErrorMsg = (event) => {
-   
-    target = event.target;
-    if (target.value === '' || event.target.classList.contains('isCheck')) {
-        console.log(target.value === '')
-        // let error = target.nextSibling;
-        let inputWrap = target.parentElement;
-        let children = inputWrap.children;
-        for(let key in children){
-            console.log(children[key])
-            // if(children[key].classList.contains('error')){
-                // error.remove(inputWrap);
-                if(children[key].className === 'error'){
-                    console.log(children[key])
-                   console.log(children[key].remove(inputWrap));
-                //    error.remove(inputWrap);
-
-                }
-            // }     
-        }    
-        // console.log(children)
-        // console.log(children.classList.contains('error'))
-        // error.remove(inputWrap);
-    }
+console.log('func del')
+target = event.target;
+target.classList.remove('invalid');
+target.classList.add('isCheck');
+if (target.value === '' || event.target.classList.contains('isCheck')) {
+    let inputWrap = target.parentElement;
+    let error = inputWrap.querySelectorAll('.error');
+    error.forEach(item => {
+        item.remove(inputWrap)
+    });
 }
+}
+
 ///// Формирует вывод ошибки
-let checkValidForm = (event, error) => {
-    let target = event.target;
+let showErrorMsg = (event, error) => {
+console.log(error)
+let errorMsg = error;
+console.log(errorMsg)
+let target = event.target;
+target.classList.remove('isCheck');
+target.classList.add('invalid');
+let inputWrap = target.parentElement;
+if (!inputWrap.querySelector('.error')) {
     let div = document.createElement('div');
-    div.className = 'error';
-    // div.style.position = 'absolute';
-    // div.style.right = '50%';
-    // div.style.bottom = '35%';
-    div.innerHTML = error;
+div.className = 'error';
+    div.innerHTML = errorMsg;
     target.after(div);
-    event.target.classList.remove('isCheck');
-}
-inputs.forEach(item => {
-    console.log(item)
-    item.addEventListener('input', (event) => {
-        delErrorMsg(event);
-    })
-})
+}else{
+    let div = inputWrap.querySelector('.error')
+    div.innerHTML = errorMsg; 
+    target.after(div);
 
+}
+
+}
 
 /////Проверка поля имени
+// let chackOnLetter = (name) => {
+//     let arr = [];
+//     name.split('').forEach(item => {
+//         // arr.push(parseInt(item));
+//         if(!isNaN(parseInt(item))){
+//             return 1;
+//         }
+//     })
+// }
+
 let checkValidInputName = (event, name) => {
-    name = name.value;
-    name = name.trim();
-    let error = '';
-    if (name.length < 2 || !name.length > 20 || name === '') {
-        checkValidForm(event, 'Введите корректное имя!');
-    } else {
-        event.target.classList.add('isCheck');
-    }
-    name.split('').forEach(item => {
-        if (!isNaN(parseInt(item))) {
-            checkValidForm(event, 'Имя не моэет содержать цыфры');
-        } else {
-            event.target.classList.add('isCheck');
-        }
-    })
+name = name.value;
+name = name.trim();
+if (name.length !== 0 && name.length < 2 || name.length > 20) {
+    showErrorMsg(event, 'Введите корректное имя!');
+}else if(name.match(/\d/)){
+    showErrorMsg(event, 'Имя не может содержать цыфры');
+}else {
+    delErrorMsg(event);
+}
 }
 nameInput.addEventListener('change', (event) => {
-    checkValidInputName(event, nameInput);
+checkValidInputName(event, nameInput);
 })
 
 
 
 //////Проверка поля емаил
 let checkValidInputEmail = (event, email) => {
-    console.log(event)
-    email = email.value;
-    email = email.trim();
-    if (email.indexOf('@') === -1 || email.indexOf('.') === -1) {
-        checkValidForm(event, 'Поле должно содержать @ и .');
-    } else {
-        event.target.classList.add('isCheck');
-    }
+console.log(event)
+email = email.value;
+email = email.trim();
+if (email.indexOf('@') === -1 || email.indexOf('.') === -1) {
+    showErrorMsg(event, 'Поле должно содержать @ и .');
+} else {
+    delErrorMsg(event);
+}
 }
 emailInput.addEventListener('change', (event) => {
-    checkValidInputEmail(event, emailInput);
+checkValidInputEmail(event, emailInput);
 })
 
 
 
 ////Проверка поля телефон
 let checkValidInputPhone = (event, phone) => {
-    // phone = phone.value;
-    // phone = phone.trim();
-    // let rep = /[\.;":'a-zA-Zа-яА-Я]/;
-    // console.log(phone.value.match(rep))
-    let regexp = /\d/g;
-    if (!phone.value.match(regexp)) {
-        phone.value = '';
-        checkValidForm(event, 'Номер не должен содержать букв');
-    }else {
-        phone.value = phone.value.match(regexp).join('');
-        event.target.classList.add('isCheck');
-    }
+// phone = phone.value;
+// phone = phone.trim();
+let regexp = /[\.;":'a-zA-Zа-яА-Я]/;
+// let regexp = /\d/g;
+if (phone.value.match(regexp)) {
+    phone.value = '';
+    showErrorMsg(event, 'Номер не должен содержать букв');
+}else if(phone.value.length !== 12){
+    showErrorMsg(event, 'Введите корректный номер');
+}else {
+    // phone.value = phone.value.match(regexp).join('');
+    delErrorMsg(event);
+}
 }
 phoneInput.addEventListener('input', (event) => {
-    checkValidInputPhone(event, phoneInput);
+checkValidInputPhone(event, phoneInput);
 })
 
 
 ////Проверка поля msg
 let checkValidInputMsg = (event, msg) => {
-    msg = msg.value;
-    msg = msg.trim();
-    console.log(msg)
-    if (msg.length < 15 && msg.length !==0) {
-        checkValidForm(event, 'Опишите проблему максимально развернуто');
-    }else {
-        event.target.classList.add('isCheck');
-    }
+msg = msg.value;
+msg = msg.trim();
+console.log(msg)
+if (msg.length < 25 && msg.length !== 0) {
+    showErrorMsg(event, 'Опишите проблему максимально развернуто');
+} else {
+    delErrorMsg(event);
 }
-msgArea.addEventListener('change', (event) => {
-    checkValidInputMsg(event, msgArea);
+}
+msgArea.addEventListener('input', (event) => {
+checkValidInputMsg(event, msgArea);
 })
 
+btn.addEventListener('click', (event) => {
+event.preventDefault();
+console.log('kjjkk')
+inputs.forEach(item => {
+    if(item.value === '' || msgArea.value === '' || item.classList.contains('invalid') || msgArea.classList.contains('invalid')){
+        console.log(item.value.length)
+        showErrorMsg(event, 'Все поля должны быть заполнены');
+    }else{
+        delErrorMsg(event);
+    }
+})
+})
